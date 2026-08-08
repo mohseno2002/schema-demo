e0c38ad7deeb4947bc2d585db29bc6589965536a
mohseno2002 <mohseno2002@gmail.com>
2026-08-08 16:57:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var VERSION = "schema-demo-v1.01";
+var VERSION = "schema-demo-v1.02";
 var SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
